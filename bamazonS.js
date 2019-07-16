b1f03ba2c3926b411b73a653f2c7c4ddc83bc5ab
bugzweiser <bugzweiser@gmail.com>
2019-07-16 00:11:53 -0700

    project completions, need to add videos

--- bamazonS.js
+++ bamazonS.js
@@ -37,7 +37,7 @@ function userInput() {
                 displayAll();
                 break;
             case 'View Total Sales of an Individual Department':
-                displaySelected1();
+                displaySelected();
                 break;
             case 'Exit':
                 connection.end();
@@ -45,6 +45,7 @@ function userInput() {
     });
 };
 
+// display all product sales
 function displayAll() {
     var query = "Select * FROM departments INNER JOIN products ON departments.item_id = products.item_id";
     connection.query(query, function(err, res) {
@@ -55,17 +56,30 @@ function displayAll() {
         });
         for (var i = 0; i < res.length; i++) {
 
+            // overhead cost calculation (all products)
             var val = res[i].over_head_costs;
             var rawVal = val - (val * .25);
             var realCost = rawVal.toFixed(2);
 
-            var profitRaw = val - realCost;
-            var salesRaw = res[i].product_sales;
-            var calc = profitRaw * salesRaw
-            var profit = calc.toFixed(2);
+            // debugging
+            // console.log("Raw: " + val + " Row= " + [i]);
+            // console.log("Real: " + realCost + " Row= " + [i]);
+
+            //profit calculation (all products)
+            var calcu = res[i].product_sales / res[i].price;
+            var calcu2 = calcu * realCost;
+            var realCalcu = calcu2.toFixed(2);
+            var profitCalc = res[i].product_sales - realCalcu;
+            var profitReal = profitCalc.toFixed(2);
+
+            // debugging
+            // console.log("Raw Sales: " + res[i].product_sales + " Row= " + [i]);
+            // console.log("Raw Calc: " + realCalcu + " Row= " + [i]);
+            // console.log("Profit: " + profitReal + " Row= " + [i]);
+
 
             displayTable.push(
-                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].department_id, realCost, res[i].product_sales, profit]
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].department_id, realCost, res[i].product_sales, profitReal]
             );
         }
         console.log(displayTable.toString());
@@ -73,6 +87,76 @@ function displayAll() {
     });
 }
 
+// function displaySelected() {
+//     inquirer.prompt([{
+
+//             name: "dept",
+//             type: "list",
+//             message: "Please select a department.",
+//             choices: ["School Supply", "Electronics", "Books"]
+//         },
+
+//     ]).then(function(input) {
+//         // debugging
+//         // console.log("You Selected " + input.dept + " department");
+//         // console.log("Input is a: " + typeof input.dept);
+
+//         if (input.dept === "School Supply") {
+//             var choice = 1
+//         }
+//         if (input.dept === "Electronics") {
+//             var choice = 2
+//         }
+//         if (input.dept === "Books") {
+//             var choice = 3
+//         };
+
+//         // debugging
+//         // console.log("Your Choice= " + choice);
+
+//         var query = "Select * FROM departments INNER JOIN products ON departments.item_id = products.item_id WHERE departments.department_id= " + choice;
+
+//         connection.query(query, function(err, res) {
+//             if (err) throw err;
+//             var displayTable = new Table({
+//                 head: ["Item ID", "Department Name", "Overhead Cost", "Product Sales", "Total Profit"],
+//                 colWidths: [16, 20, 15, 15, 15]
+//             });
+//             for (var i = 0; i < res.length; i++) {
+//                 // debugging
+//                 // console.log(res[i].product_sales);
+
+//                 // var query2 = "SELECT SUM (" + res[i].over_head_costs + ") FROM departments WHERE departments.department_id= " + choice;
+
+//                 var val = res[i].over_head_costs;
+//                 var rawVal = val - (val * .25);
+//                 var realCost = rawVal.toFixed(2);
+
+//                 var profitRaw = val - realCost;
+//                 var salesRaw = res[i].product_sales;
+//                 var calc = profitRaw * salesRaw
+//                 var profit = calc.toFixed(2);
+
+//                 // var totalSales = connection.query(query2, function(err, res) {
+//                 //     if (err) throw err;
+//                 // });
+//                 // console.log(totalSales);
+
+//                 var totalProfit;
+//                 var totalOverhead;
+
+//                 displayTable.push(
+//                     // [res[i].department_id, res[i].department_name, res[i].product_sales]
+//                     [res[i].item_id, res[i].department_name, realCost, res[i].product_sales, profit]
+//                 );
+//             }
+//             console.log(displayTable.toString());
+//             userInput();
+//         });
+//     });
+// }
+
+// display individual departments
 function displaySelected() {
     inquirer.prompt([{
 
@@ -97,76 +181,6 @@ function displaySelected() {
             var choice = 3
         };
 
-        // debugging
-        // console.log("Your Choice= " + choice);
-
-        var query = "Select * FROM departments INNER JOIN products ON departments.item_id = products.item_id WHERE departments.department_id= " + choice;
-
-        connection.query(query, function(err, res) {
-            if (err) throw err;
-            var displayTable = new Table({
-                head: ["Item ID", "Department Name", "Overhead Cost", "Product Sales", "Total Profit"],
-                colWidths: [16, 20, 15, 15, 15]
-            });
-            for (var i = 0; i < res.length; i++) {
-                // debugging
-                // console.log(res[i].product_sales);
-
-                // var query2 = "SELECT SUM (" + res[i].over_head_costs + ") FROM departments WHERE departments.department_id= " + choice;
-
-                var val = res[i].over_head_costs;
-                var rawVal = val - (val * .25);
-                var realCost = rawVal.toFixed(2);
-
-                var profitRaw = val - realCost;
-                var salesRaw = res[i].product_sales;
-                var calc = profitRaw * salesRaw
-                var profit = calc.toFixed(2);
-
-                // var totalSales = connection.query(query2, function(err, res) {
-                //     if (err) throw err;
-                // });
-                // console.log(totalSales);
-
-                var totalProfit;
-                var totalOverhead;
-
-                displayTable.push(
-                    // [res[i].department_id, res[i].department_name, res[i].product_sales]
-                    [res[i].item_id, res[i].department_name, realCost, res[i].product_sales, profit]
-                );
-            }
-            console.log(displayTable.toString());
-            userInput();
-        });
-    });
-}
-
-function displaySelected1() {
-    inquirer.prompt([{
-
-            name: "dept",
-            type: "list",
-            message: "Please select a department.",
-            choices: ["School Supply", "Electronics", "Books"]
-        },
-
-    ]).then(function(input) {
-        // debugging
-        // console.log("You Selected " + input.dept + " department");
-        // console.log("Input is a: " + typeof input.dept);
-
-        if (input.dept === "School Supply") {
-            var choice = 1
-        }
-        if (input.dept === "Electronics") {
-            var choice = 2
-        }
-        if (input.dept === "Books") {
-            var choice = 3
-        };
-
-        // var query = "Select * FROM departments INNER JOIN products ON departments.item_id = products.item_id WHERE departments.department_id= " + choice;
         var query = "SELECT * FROM departments INNER JOIN products ON departments.item_id = products.item_id WHERE departments.department_id= " + choice;
 
         connection.query(query, function(err, res) {
@@ -177,65 +191,62 @@ function displaySelected1() {
             const salePrice = [];
             const prodSales = [];
 
-
-
             const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
             for (i = 0; i < res.length; i++) {
+                // response
                 // console.log(res[i].price);
-                var saleData = res[i].price;
+
+                var saleData = res[i].over_head_costs * res[i].quantity;
                 var prodData = res[i].product_sales;
                 prodSales.push(prodData);
                 salePrice.push(saleData);
 
             }
-            // totals calculations
+
+            //department name
+            //department name table population upon request
+            if (choice === 1) {
+                var deptName = "School Supply"
+            }
+            if (choice === 2) {
+                var deptName = "Electronics"
+            }
+            if (choice === 3) {
+                var deptName = "Books"
+            }
+
+            // department total calculations
 
             // sale price
             // console.log(salePrice);
             var saleOut = (salePrice.reduce(reducer));
+            // var overHead = saleOut - (saleOut * .25)
             var totalPrice = saleOut.toFixed(2);
-            console.log("Total Price: " + totalPrice);
+            console.log("Total Overhead: " + totalPrice);
 
-            //product sales
+            // product sales (minus 75% overhead)
             // console.log(prodSales);
             var prodOut = (prodSales.reduce(reducer));
-            var totalProd = prodOut.toFixed(2);
-            console.log("Total Sales: " + totalProd);
+            var markUp = prodOut - (prodOut * .75);
+            var totalProd = markUp.toFixed(2);
+            // debugging
+            // console.log("Total Sales: " + totalProd);
+
+            //total Profit
+            var profitRaw = totalProd - totalPrice;
+            var profitReal = profitRaw.toFixed(2);
+            // debugging
+            // console.log("Total Profit: " + profitReal);
 
             var displayTable = new Table({
                 head: ["Department Name", "Overhead Cost", "Product Sales", "Total Profit"],
-                colWidths: [16, 20, 15, 15, 15]
+                colWidths: [25, 15, 15, 15]
             });
-            // for (var i = 0; i < res.length; i++) {
-            //     // debugging
-            //     // console.log(res[i].product_sales);
-
-            //     // var query2 = "SELECT SUM (" + res[i].over_head_costs + ") FROM departments WHERE departments.department_id= " + choice;
-
-            //     var val = res[i].over_head_costs;
-            //     var rawVal = val - (val * .25);
-            //     var realCost = rawVal.toFixed(2);
-
-            //     var profitRaw = val - realCost;
-            //     var salesRaw = res[i].product_sales;
-            //     var calc = profitRaw * salesRaw
-            //     var profit = calc.toFixed(2);
-
-            //     // var totalSales = connection.query(query2, function(err, res) {
-            //     //     if (err) throw err;
-            //     // });
-            //     // console.log(totalSales);
-
-            //     var totalProfit;
-            //     var totalOverhead;
-
-            // displayTable.push(
-            //     // [res[i].department_id, res[i].department_name, res[i].product_sales]
-            //     [res[i].item_id, totalPrice, realCost, res[i].product_sales, profit]
-            // );
-            // }
-            // console.log(displayTable.toString());
+            displayTable.push(
+                [deptName, totalPrice, totalProd, profitReal]
+            );
+            console.log(displayTable.toString());
             userInput();
         });
     });
